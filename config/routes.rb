e8e67653927cfee60f44aaf3152c5ae786c5183e
Benjamin Beguin <beguin.ben@gmail.com>
2016-11-26 15:11:36 +0100

    master[init] code style for a better world

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
 
+    root 'welcome#index' #home
 
-  root 'welcome#index'
+    #-------------- POST -------------#
+    
+    resources :posts
 
-  resources :posts
-  #-------------- USER -------------#
-  devise_for :users, controllers: {
-  		registrations: 'users/registrations',
+    #-------------- USER -------------#
+
+    devise_for :users, controllers: {
+        registrations: 'users/registrations',
         sessions: 'users/sessions'
-      }
+    }
 
-      devise_scope :user do
-          put 'users/upload', to: 'devise/registrations#upload', as: :upload
-      end
+    devise_scope :user do
+        put 'users/upload', to: 'devise/registrations#upload', as: :upload
+    end
       
-  get 'users/' => 'users#index'
-  #put 'users/upload' => 'users/registrations#upload'
+    get 'users/' => 'users#index'
 
-  #----------------------------------#
+    #----------------------------------#
 
-  get "about-this-class", to: "pages#about"
 end
