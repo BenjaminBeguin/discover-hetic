f90c9bdd6d6ba0e5df006bc96df25e59dcae69ea
Benjamin Beguin <beguin.ben@gmail.com>
2016-11-29 12:06:38 +0100

    dev[User Test] test for edit a post

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,8 +18,69 @@ describe UsersController do
 	  expect(found_post.content).to eq("contenu")
 	end
 
+	it "a not login user cant edit a post" do
+	  user = User.create(name: "Jean", email: "jean@gmail.com", password: "password")
+	  user.save!
+	  post = Post.create(title: "Titre", content: "contenu", url: "https://www.google.fr/" , user_id: user.id)
+	  post.save!
 
-  	login_user
+	  found_user = User.last
+	  found_post = Post.last
+	  expect(found_user.name).to eq("Jean")
+	  expect(found_user.email).to eq("jean@gmail.com")
+
+	  expect(found_post.title).to eq("Titre")
+	  expect(found_post.user.name).to eq("Jean")
+	  expect(found_post.content).to eq("contenu")
+
+	  get :edit_user_post , { id: post.id}
+	  expect(response).to_not have_http_status(:success)
+	  
+	
+	end
+
+	login_user
+
+	it "a login user can edit him post" do
+		post = Post.create(title: "Titre", content: "contenu", url: "https://www.google.fr/" , user_id: subject.current_user.id)
+		post.save!
+
+		found_post = Post.last
+
+
+		expect(found_post.title).to eq("Titre")
+		expect(found_post.content).to eq("contenu")
+
+		expect(subject.current_user).to_not eq(nil)
+
+		puts subject.current_user.id
+
+		get :edit_user_post , { id: post.id }
+
+		expect(response).to have_http_status(:success)
+	end
+
+	it "a login user can not edit not him post" do
+		post = Post.create(title: "Titre", content: "contenu", url: "https://www.google.fr/" , user_id: subject.current_user.id + 1)
+		post.save!
+
+		found_post = Post.last
+
+
+		expect(found_post.title).to eq("Titre")
+		expect(found_post.content).to eq("contenu")
+
+		expect(subject.current_user).to_not eq(nil)
+
+		puts subject.current_user.id
+
+		get :edit_user_post , { id: post.id }
+
+		expect(response).to_not have_http_status(:success)
+	end
+
+
+  	
 
 	it "should have a current_user" do
 		# note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
