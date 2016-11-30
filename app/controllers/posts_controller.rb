be5fa2376c358dd571eb51bc2774862a239abf59
Benjamin Beguin <beguin.ben@gmail.com>
2016-11-30 23:13:05 +0100

    dev[Post] organiser fichier - all works here

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,36 +2,37 @@ class PostsController < ApplicationController
     def index
         @connected = user_signed_in?
         @posts = Post.paginate(:page => params[:page], :per_page => 6)
-          @day = Time.now
-        @post = top_of_the_day(@day);
+        @day = Time.now
+        @top_post = top_of_the_day(@day);
+    end
+    
+    def top_of_the_day(date)
+        @top_post = Post.where(created_at: date.midnight..date.end_of_day).order(vote: :desc).first
     end
 
     def show
         @connected = user_signed_in?
         @post = Post.find_by_id(params[:id]) or not_found
         @comments = Comment.where(post_id: params[:id])
-
-      
     end
 
-    def top_of_the_day(date)
-        @connected = user_signed_in?
-        @post_first = Post.where(created_at: date.midnight..date.end_of_day).order(vote: :desc).first or not_found
-    end
 
 
     #------------------ filter and order -----------------#
     def category
         @category_slug = params[:slug];
-        if Category.where(slug: @category_slug).first
-            @category = Category.where(slug: @category_slug).first;
+        @category = Category.where(slug: @category_slug).first
+
+        if  !@category.blank?
+
             if params[:orderby] == 'top'
                 @posts = Post.where(category_id: @category.id).order(vote: :desc)
             else
                 @posts = Post.where(category_id: @category.id).order(created_at: :desc)
             end
+
             #--- get the first by vote --#
-            @post_first = Post.where(category_id: @category.id).order(vote: :desc).first
+            @top_post = Post.where(category_id: @category.id).order(vote: :desc).first
         else
             redirect_to action: "index"
         end
