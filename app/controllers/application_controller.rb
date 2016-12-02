89029ee35bcdd76e3a6999531593ff63deaf4af3
Benjamin Beguin <beguin.ben@gmail.com>
2016-12-02 10:55:06 +0100

    dev[Global] add catgorie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_current_user
+  before_filter :all_categories
+
+  def all_categories
+    @categories = Category.all
+  end
 
   def set_current_user
     Post.current_user = current_user
