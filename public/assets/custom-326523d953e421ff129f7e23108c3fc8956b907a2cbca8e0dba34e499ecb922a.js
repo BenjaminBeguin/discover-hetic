3a192a3de457ca4c308b0dfdf7229fbfccad6b04
stephen-richard <stephen.richard@hetic.net>
2016-12-05 11:18:48 +0100

    Start posts list style

---
+++ public/assets/custom-326523d953e421ff129f7e23108c3fc8956b907a2cbca8e0dba34e499ecb922a.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".search-button").on("click",function(){var a=$(".search-form");a.hasClass("active")||(event.preventDefault(),a.addClass("active"))}),$("main").on("click",function(){$(".search-form").hasClass("active")&&$(".search-form").removeClass("active")})});
