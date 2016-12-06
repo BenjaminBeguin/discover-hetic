2ee1fe67c502cc689c4ceffb79ebfb7881d6a345
Benjamin Beguin <beguin.ben@gmail.com>
2016-12-06 09:26:30 +0100

    add flash when trying to vote when ur not connected

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,12 @@ class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+    if params[:error]
+      flash[:error] = params[:error]
+    end
+  end
 
   # POST /resource/sign_in
   # def create
