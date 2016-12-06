9568226dd1684d89cd5c9c3012abdddacbc5109c
Benjamin Beguin <beguin.ben@gmail.com>
2016-12-06 17:34:05 +0100

    try fix mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.default_url_options = { :host => 'discover-hetic.herokuapp.com' }
-
 # Initialize the Rails application.
 Rails.application.initialize!
