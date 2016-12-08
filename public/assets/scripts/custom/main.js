c72f7c17ab4aff2f18c87b377548fa76339f9764
stephen-richard <stephen.richard@hetic.net>
2016-12-08 15:00:17 +0100

    Avatar upload image preview

--- public/assets/scripts/custom/main.js
+++ public/assets/scripts/custom/main.js
@@ -16,12 +16,19 @@ $(document).ready(function(){
 		}
 	})
 
+	var $currentImage = $('#current-image');
+
 	$('#user_avatar').on('change', function (e) {
 		
-		$val = $(e.target).val();
+		if(this.files && this.files[0]) {
+			var reader = new FileReader();
 
-		if($val) {
-			$('.upload-avatar-check').show();
+			reader.onload = function(e) {
+				// $currentImage[0].src = e.target.result
+				$('#avatar_input').css('background-image', 'url('+e.target.result+')')
+			}
+			reader.readAsDataURL(this.files[0]);
+			$currentImage.css('opacity', '1');
 		}
 	});
 
